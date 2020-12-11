1f628471139e34741a5d1d901e6cc17f722eb419
Delgado Trueba <delgadotrueba@outlook.es>
2020-12-11 23:25:15 +0100

    solhint rule

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.7.0;
 
 contract HelloWorld {
-    string name = "hola";
+    string private name = "hola";
 
     function greeding() external view returns (string memory) {
         return name;
