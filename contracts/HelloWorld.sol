4f5dea2ea98ed6d7b3219cc490eebf072be4b116
Delgado Trueba <delgadotrueba@outlook.es>
2020-12-12 16:25:32 +0100

    fea: (HelloWorld) Update Contract
    - add NewName event
    - add setName function
    
    Refs #3

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,9 +2,16 @@
 pragma solidity 0.7.0;
 
 contract HelloWorld {
-    string private name = "hola";
+    event NewName(string name);
+
+    string public name = "hola";
 
     function greeding() external view returns (string memory) {
         return name;
+    }
+
+    function setName(string memory _name) public {
+        name = _name;
+        emit NewName(_name);
     }
 }
